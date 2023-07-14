dad80cbcc217bec943f3d26fbb958282bbaaca01
Ylan Anc <ylansachapierre@gmail.com>
2023-07-14 10:01:22 +0200

    problemes réglées

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-
+const bodyParser = require ('body-parser')
 const mongoose = require('mongoose');
 
 const app = express();
+
+const path = require('path');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://Naly:taofiasco@p7.2gmbuyd.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -12,14 +16,18 @@ mongoose.connect('mongodb+srv://Naly:taofiasco@p7.2gmbuyd.mongodb.net/?retryWrit
 
 
 app.use((req, res, next) => {
-   res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-   next();
- });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
-app.use('/api/books', (req,res) => {
-   res.json({ book : '1984' , book2 : '1974'} )
-})
+app.use(bodyParser.json());
+
+
+app.use('/api/books', stuffRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 
 module.exports = app ;
---
+++ backend/controllers/stuff.js
@@ -0,0 +1,96 @@
+const Book = require('../models/book');
+const fs = require('fs');
+
+exports.createBook = (req, res, next) => {
+  console.log(req.body.book);
+  const thingBook = JSON.parse(req.body.book);
+  delete thingBook._id;
+  // delete thingBook._userId;
+  console.log(thingBook);
+  const book = new Book({
+      ...thingBook,
+      userId: req.auth.userId,
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  });
+  console.log(req.auth.userId);
+  console.log(book);
+  book.save()
+  .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
+  .catch(error => { res.status(400).json( {error})})
+};
+ 
+
+
+exports.getOneBook = (req, res, next) => {
+  Book.findOne({
+    _id: req.params.id
+  }).then(
+    (thing) => {
+      res.status(200).json(thing);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.modifyBook = (req, res, next) => {
+  const thingBook = req.file ? {
+      ...JSON.parse(req.body.thing),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
+
+  delete thingBook._userId;
+  Book.findOne({_id: req.params.id})
+      .then((thing) => {
+          if (thing.userId != req.auth.userId) {
+              res.status(401).json({ message : 'Not authorized'});
+          } else {
+              Book.updateOne({ _id: req.params.id}, { ...thingBook, _id: req.params.id})
+              .then(() => res.status(200).json({message : 'Objet modifié!'}))
+              .catch(error => res.status(401).json({ error }));
+          }
+      })
+      .catch((error) => {
+          res.status(400).json({ error });
+      });
+};
+
+
+
+exports.deleteBook = (req, res, next) => {
+  Book.findOne({ _id: req.params.id})
+      .then(thing => {
+          if (thing.userId != req.auth.userId) {
+              res.status(401).json({message: 'Not authorized'});
+          } else {
+              const filename = thing.imageUrl.split('/images/')[1];
+              fs.unlink(`images/${filename}`, () => {
+                  Book.deleteOne({_id: req.params.id})
+                      .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
+                      .catch(error => res.status(401).json({ error }));
+              });
+          }
+      })
+      .catch( error => {
+          res.status(500).json({ error });
+      });
+};
+
+
+exports.getAllBooks = (req, res, next) => {
+  Book.find().then(
+    (books) => {
+      res.status(200).json(books);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
